35db25f8ac0d80c9a56802814cd7af28b9802ebb
lola200000 <mirzoyanlola@gmail.com>
2025-09-29 07:54:40 +0200

    Update main.php

--- main.php
+++ main.php
@@ -1,4 +1,38 @@
 <?php
+declare(strict_types=1);
+
+require_once DIR . '/Animal.php';
+require_once DIR . '/Visiteur.php';
+require_once DIR . '/Zoo.php';
+
+// Création du zoo
+$zoo = new Zoo();
+
+// Livraison d'animaux (avant l'ouverture)
+$zoo->livraison([
+    new Lion("Concon"),
+    new Zebre("Rayures")
+]);
+
+// Création des visiteurs (tes 10 noms)
+$visiteurs = [
+    new Visiteur("Loïc"),
+    new Visiteur("Yannick"),
+    new Visiteur("Yassine"),
+    new Visiteur("Enzo"),
+    new Visiteur("Pierre"),
+    new Visiteur("collègues"),
+    new Visiteur("petits enfants"),
+    new Visiteur("Wahid"),
+    new Visiteur("couple amoureux"),
+    new Visiteur("la famille"),
+];
+
+// Vente des billets
+$zoo->vendreBillet($visiteurs);
+
+// Ouverture du zoo
+$zoo->ouvrirLesPortes();
 
 
 ?>
